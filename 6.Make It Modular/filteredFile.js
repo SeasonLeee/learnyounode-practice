f89a4ac0722b508bda130c1142ee450e0c918385
Benjamin <benjamin.voila@gmail.com>
2019-10-24 11:46:28 +0800

    finish the 06, I think😁

--- 6.Make It Modular/filteredFile.js
+++ 6.Make It Modular/filteredFile.js
@@ -1,29 +1,31 @@
 
 /**
- * 
- * @param {path to the file} filePath 
- * @param {target file name extension} ext 
- * @param {callback function to handle the former two} callback 
- * 
+ *
+ * @param {path to the file} filePath
+ * @param {target file name extension} ext
+ * @param {callback function to handle the former two} callback
+ *
  */
 function filterFile(filePath, ext, callback) {
     const fs = require('fs');
     const path = require('path');
-    
+
     // const processArgv = process.argv;
-    
+
     const fb = filePath;
     const targetType = ext;
-    
-    // fs.readdir(fb, (err, res) => {
-    //     if (err) console.error(err);
-    
-    //     let result = res.filter(e => path.extname(e) === '.' + targetType);
-    
-    //     result.forEach(e => console.log(e));
-    // }); 
-    
-    fs.readdir(fb, callback(err, res))
+
+    fs.readdir(fb, (err, res) => {
+        if (err) callback(err);
+
+        let result = res.filter(e => path.extname(e) === '.' + targetType);
+
+        // result.forEach(e => callback(e));
+
+        callback(null, result);
+    });
+
+    // fs.readdir(fb, callback(err, res))
 }
 
 module.exports = filterFile;
--- 6.Make It Modular/testFilterFile.js
+++ 6.Make It Modular/testFilterFile.js
@@ -1,17 +1,35 @@
 // function filterFile(params) {
 //     const fs = require('fs');
 //     const path = require('path');
-    
+
 //     const processArgv = process.argv;
-    
+
 //     const filePath = processArgv[2];
 //     const targetType = processArgv[3];
-    
+
 //     fs.readdir(filePath, (err, res) => {
 //         if (err) console.error(err);
-    
+
 //         let result = res.filter(e => path.extname(e) === '.' + targetType);
-    
+
 //         result.forEach(e => console.log(e));
-//     });    
+//     });
 // }
+const filterFile = require('./filteredFile');
+
+// console.log(filterFile());
+
+const processArgv = process.argv;
+
+const filePath = processArgv[2];
+const targetType = processArgv[3];
+
+filterFile(filePath, targetType, (err, res) => {
+    if (err) console.error(err);
+
+    res.forEach(element => {
+        console.log(element)
+    });
+
+    // console.log(typeof res);
+})
