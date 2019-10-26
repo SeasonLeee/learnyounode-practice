2746893d258b8ef137950b950d9276281cac55bf
Benjamin <jijileeeeeee@gmail.com>
2019-10-26 16:08:01 +0800

    add official solutions to the code for note

--- 6.Make It Modular/filteredFile.js
+++ 6.Make It Modular/filteredFile.js
@@ -22,10 +22,34 @@ function filterFile(filePath, ext, callback) {
 
         // result.forEach(e => callback(e));
 
-        return callback(null, result);
+        callback(null, result);
     });
 
     // fs.readdir(fb, callback(err, res))
 }
 
 module.exports = filterFile;
+
+
+// Here's the official solution in case you want to compare notes:
+// ─────────────────────────────────────────────────────────────────────────────
+//  _/usr/local/lib/node_modules/learnyounode/exercises/make_it_modular/soluti
+//  on/solution_filter.js_ :
+
+
+//     var fs = require('fs')
+//     var path = require('path')
+    
+//     module.exports = function (dir, filterStr, callback) {
+//       fs.readdir(dir, function (err, list) {
+//         if (err) {
+//           return callback(err)
+//         }
+    
+//         list = list.filter(function (file) {
+//           return path.extname(file) === '.' + filterStr
+//         })
+    
+//         callback(null, list)
+//       })
+//     }
--- 6.Make It Modular/testFilterFile.js
+++ 6.Make It Modular/testFilterFile.js
@@ -1,23 +1,4 @@
-// function filterFile(params) {
-//     const fs = require('fs');
-//     const path = require('path');
-
-//     const processArgv = process.argv;
-
-//     const filePath = processArgv[2];
-//     const targetType = processArgv[3];
-
-//     fs.readdir(filePath, (err, res) => {
-//         if (err) console.error(err);
-
-//         let result = res.filter(e => path.extname(e) === '.' + targetType);
-
-//         result.forEach(e => console.log(e));
-//     });
-// }
 const filterFile = require('./filteredFile');
-
-// console.log(filterFile());
 
 const processArgv = process.argv;
 
@@ -27,9 +8,27 @@ const targetType = processArgv[3];
 filterFile(filePath, targetType, (err, res) => {
     if (err) console.error(err);
 
-    res.forEach(element => {
-        console.log(element)
-    });
+    res.forEach(element => console.log(element));
 
-    // console.log(typeof res);
 })
+
+// Here's the official solution in case you want to compare notes:
+
+// ─────────────────────────────────────────────────────────────────────────────
+//  _/usr/local/lib/node_modules/learnyounode/exercises/make_it_modular/soluti
+//  on/solution.js_ :
+
+
+// var filterFn = require('./solution_filter.js')
+// var dir = process.argv[2]
+// var filterStr = process.argv[3]
+
+// filterFn(dir, filterStr, function (err, list) {
+//     if (err) {
+//         return console.error('There was an error:', err)
+//     }
+
+//     list.forEach(function (file) {
+//         console.log(file)
+//     })
+// })
