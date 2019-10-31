2a46234ccae84271eb74e666d75510973c413729
Benjamin <jijileeeeeee@gmail.com>
2019-10-31 16:59:41 +0800

    add official note for later study

--- 11.File Server/fileServer.js
+++ 11.File Server/fileServer.js
@@ -10,4 +10,21 @@ const server = http.createServer((req, res) => {
     res.write(stuffStream.read());
 });
 
-server.listen(port)
+server.listen(port);
+
+// Here's the official solution in case you want to compare notes:
+
+// ─────────────────────────────────────────────────────────────────────────────
+
+//     var http = require('http')
+//     var fs = require('fs')
+    
+//     var server = http.createServer(function (req, res) {
+//       res.writeHead(200, { 'content-type': 'text/plain' })
+    
+//       fs.createReadStream(process.argv[3]).pipe(res)
+//     })
+    
+//     server.listen(Number(process.argv[2]))
+
+// ─────────────────────────────────────────────────────────────────────────────
