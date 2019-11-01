9e214630734efa4c61bd3740001ebf2fcbc006eb
Benjamin <jijileeeeeee@gmail.com>
2019-11-02 00:27:17 +0800

    well, this time is definitely finish and I have so many questions....

--- 12.HTTP UPPERCASE/httpUppercase.js
+++ 12.HTTP UPPERCASE/httpUppercase.js
@@ -3,15 +3,36 @@ const http = require('http');
 const port = process.argv[2];
 
 const server = http.createServer((req, res) => {
-
-    // console.log(req);
-
     if (req.method === 'POST') {
-        req.on('data', (data) => console.log(data.toString().toUpperCase()));
+        req.on('data', (data) => {
+            res.write(data.toString().toUpperCase());
+        });
     }
-    // res.end('hahaha');
 });
 
 server.listen(port);
 
-// console.log('jianglin lalala'.toUpperCase);
+/// well, I finish this challenge and so many challenge before without
+/// actually knowing what's actually going on...
+/// what the fuck...
+
+// Here's the official solution in case you want to compare notes:
+
+// ─────────────────────────────────────────────────────────────────────────────
+
+//     var http = require('http')
+//     var map = require('through2-map')
+    
+//     var server = http.createServer(function (req, res) {
+//       if (req.method !== 'POST') {
+//         return res.end('send me a POST\n')
+//       }
+    
+//       req.pipe(map(function (chunk) {
+//         return chunk.toString().toUpperCase()
+//       })).pipe(res)
+//     })
+    
+//     server.listen(Number(process.argv[2]))
+
+// ─────────────────────────────────────────────────────────────────────────────
