71db8ddc135c3cf0fddfde08ea5365d3ebfcb670
Benjamin <jijileeeeeee@gmail.com>
2019-11-04 20:58:20 +0800

    add official solution to compare and see that hooray?😆

--- 13.HTTP JSON Api Server/httpJSONApiServer.js
+++ 13.HTTP JSON Api Server/httpJSONApiServer.js
@@ -48,3 +48,48 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port);
+
+// Here's the official solution in case you want to compare notes:
+
+// ─────────────────────────────────────────────────────────────────────────────
+
+//     var http = require('http')
+//     var url = require('url')
+    
+//     function parsetime (time) {
+//       return {
+//         hour: time.getHours(),
+//         minute: time.getMinutes(),
+//         second: time.getSeconds()
+//       }
+//     }
+    
+//     function unixtime (time) {
+//       return { unixtime: time.getTime() }
+//     }
+    
+//     var server = http.createServer(function (req, res) {
+//       var parsedUrl = url.parse(req.url, true)
+//       var time = new Date(parsedUrl.query.iso)
+//       var result
+    
+//       if (/^\/api\/parsetime/.test(req.url)) {
+//         result = parsetime(time)
+//       } else if (/^\/api\/unixtime/.test(req.url)) {
+//         result = unixtime(time)
+//       }
+    
+//       if (result) {
+//         res.writeHead(200, { 'Content-Type': 'application/json' })
+//         res.end(JSON.stringify(result))
+//       } else {
+//         res.writeHead(404)
+//         res.end()
+//       }
+//     })
+//     server.listen(Number(process.argv[2]))
+
+// ─────────────────────────────────────────────────────────────────────────────
+//  You've finished all the challenges! Hooray!
+
+// ─────────────────────────────────────────────────────────────────────────────
